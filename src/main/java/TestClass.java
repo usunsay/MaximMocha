310a1fbd836717f7b97628923382d5d5b2cdb435
usunsay <macaronfam@gmail.com>
2019-08-08 17:13:42 +0900

    add class TestClass

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,18 @@
+
+public class TestClass {
+	int i ;
+	int j;
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public int getJ() {
+		return j;
+	}
+	public void setJ(int j) {
+		this.j = j;
+	}
+	
+}
